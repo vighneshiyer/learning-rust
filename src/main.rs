7bfaeb77cb2317fce5a2e335ae1655cacb2fe3f5
Vighnesh Iyer <vighnesh.iyer@gmail.com>
2024-04-02 10:27:29 -0700

    more stuff

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,27 @@
-use comprehensive_rust::{collatz_length, fizzbuzz};
-
-fn multiply(x: i16, y: i16) -> i16 {
-    x * y
-}
-
-fn fib(n: u32) -> u32 {
-    if n <= 2 {
-        return 1;
-    } else {
-        return fib(n - 1) + fib(n - 2);
-    }
-}
-
-fn factorial(n: u32) -> u32 {
-    let mut product = 1;
-    for i in 1..=n {
-        product *= dbg!(i);
-    }
-    product
-}
+use comprehensive_rust::{collatz_length, factorial, fizzbuzz, transpose};
 
 fn main() {
     factorial(4);
     println!("{:?}", (100, 200));
     println!("{}, {}", collatz_length(1), collatz_length(3));
+
+    let mut a: [i8; 10] = [42; 10]; // fill array with literal '42'
+    a[5] = 0;
+    println!("{a:#?}");
+
+    let t: (i8, bool) = (7, true);
+    println!("t.0: {}, t.1: {}", t.0, t.1);
+
+    let primes = [2, 3, 5, 7, 11, 13, 17, 19];
+    for prime in primes {
+        for i in 2..prime {
+            assert_ne!(prime % i, 0);
+        }
+    }
+
+    let array: [[i32; 3]; 3] = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    let array = transpose(array);
+
     // let x: i8 = 13;
     // let y: i16 = 1000;
     // let _z: i32 = 1000;
